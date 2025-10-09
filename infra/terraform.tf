1802cb41c827740748de8786589caadbb69b9c2f
Wouter Heyvaert <wouterheyvaert1@gmail.com>
2025-10-09 09:28:02 +0200

    chore(infra): initialized terraform project

---
+++ infra/terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "windspotter-terraform-state"
+    key          = "terraform.tfstate"
+    region       = "eu-west-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
